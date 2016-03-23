0e9986a88320dec8a94f7accf38875f8bd658080
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-22 22:37:14 -0700

    added player explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour 
 {
+	public GameObject explosion;
+	public GameObject PlayerExplosion;
+
 	//Destroying game objects on entering boundary
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary") 
 		{
 			return;
+		}
+	//Asteroid Explosion
+		Instantiate(explosion, transform.position, transform.rotation);
+	//Player Explosion
+		if (other.tag == "Player")
+		{
+		Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
 		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
