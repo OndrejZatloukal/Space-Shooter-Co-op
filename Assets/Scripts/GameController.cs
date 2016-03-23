389d58b684e68ecadf44f672e75ba10c2536563c
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-22 23:20:52 -0700

    added spawn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,17 +5,31 @@ public class GameController : MonoBehaviour
 {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount; 
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
+	//Asteroid spawning
 	void Start ()
 	{
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnWaves ()
+	IEnumerator SpawnWaves ()
 	{
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		//Spawn Asteroid in wide range 
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
-
 }
