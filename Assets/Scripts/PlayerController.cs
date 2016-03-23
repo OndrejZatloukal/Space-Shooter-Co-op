2bf8fff63481bf4f53e63f8ce8b9bc2a6030126d
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-22 19:19:46 -0700

    added player script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	// player movement 
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+	//defines range of players movement
+		rb.position = new Vector3
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+	//ship roration
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}	
+}
