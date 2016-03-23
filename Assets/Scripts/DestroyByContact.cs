4aa11bb0c0f73ff7723ecbeece885471903caee3
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-22 22:18:39 -0700

    added creating hazards

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	//Destroying game objects on entering boundary
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	private Rigidbody rb;
+
+	//Asteroid's rotation
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
