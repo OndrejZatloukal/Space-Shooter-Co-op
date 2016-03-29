15b65a599fc03163cf3eb0ee2c31a15e85d779fc
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-28 19:56:08 -0700

    Added tracking missiles

---
+++ Assets/Scripts/EnemyScripts/DestroyByContact.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject PlayerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+		if (gameController == null) 
+		{
+			Debug.Log ("Cannot find 'GameController' Script");
+		}
+	}
+
+	//Destroying game objects on entering boundary
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy")) 
+		{
+			return;
+		}
+	//Asteroid Explosion
+		if (explosion != null)
+		{
+		Instantiate(explosion, transform.position, transform.rotation);
+		}
+	//Player Explosion
+		if (other.tag == "Player")
+		{
+		Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
+		gameController.GameOver ();
+		}
+		gameController.AddScore (scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/EnemyScripts/EvasiveManouverMissile.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvasiveManouverMissile : MonoBehaviour {
+
+
+	//Enemy flying towards movement
+
+	public float dodge;
+	public float smoothing;
+	public float tilt;
+	public Vector2 startWait;
+	public Vector2 manouverTime;
+	public Vector2 manouverWait;
+	public Boundary boundary;
+
+	private Transform playerTransform;
+
+	private float currentSpeed;
+	private float targetManouver;
+	private Rigidbody rb;
+
+	void Start () 
+	{
+		rb = GetComponent <Rigidbody> ();
+		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		currentSpeed = rb.velocity.z;
+		StartCoroutine (Evade ());
+	}
+
+	IEnumerator Evade ()
+	{
+		yield return new WaitForSeconds (Random.Range (startWait.x, startWait.y));
+
+		while (true) 
+		{
+	//		targetManouver = Random.Range (1, dodge) * -Mathf.Sign (transform.position.x);
+			targetManouver = playerTransform.position.x;
+			yield return new WaitForSeconds (Random.Range (manouverTime.x, manouverTime.y));
+			targetManouver = 0;
+			yield return new WaitForSeconds (Random.Range (manouverWait.x, manouverWait.y));
+		}
+	}
+	
+
+	void FixedUpdate () 
+	{
+		float newManouver = Mathf.MoveTowards (rb.velocity.x, targetManouver, Time.deltaTime * smoothing); 
+		rb.velocity = new Vector3 (newManouver, 0.0f, currentSpeed);
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
